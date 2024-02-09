8cdd3d3a487d5be94cfda58af85927a3773ad1d9
saufth <saufth@gmail.com>
2024-02-09 14:38:14 -0600

    Update site header background color

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -25,8 +25,8 @@ export default function SiteHeader () {
       <header
         className={cn(
           'w-full sticky top-0 left-0 z-40 border-none transition-colors duration-300 bg-black/80 backdrop-filter backdrop-saturate-150 backdrop-blur-lg transition-color',
-          isOnTop && 'bg-transparent',
-          isMenuOpen && 'bg-transparent'
+          isOnTop && 'bg-black/10',
+          isMenuOpen && 'bg-black/10'
         )}
       >
         <nav className='relative' aria-label='Emah Directory'>
